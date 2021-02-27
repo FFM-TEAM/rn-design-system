b38a6534c352217bb6232b9a83850454d15cd831
s-ong-c <somony9292@gmail.com>
2021-02-27 15:36:59 +0900

    feat: react-native components ui setup , storybook setup

---
+++ storybook/stories/Welcome/Welcome.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import {View, Text} from 'react-native';
+
+function Welcome() {
+  return (
+    <View>
+      <Text>Welcome to React Native Storybook</Text>
+    </View>
+  );
+}
+
+export default Welcome;
